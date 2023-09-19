353190a30cbffb85dd27d0a5ff3b3f164ed84328
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-20 01:26:47 +0300

    import necessary methods

--- dags/airflow_dag.py
+++ dags/airflow_dag.py
@@ -1,10 +1,16 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 
-from kafka_create_topic import create_new_topic
+from kafka_create_topic import kafka_create_topic_main
+from kafka_consumer_cassandra import kafka_consumer_cassandra_main
+from kafka_consumer_mongodb import kafka_consumer_mongodb_main
+from kafka_producer import kafka_producer_main
+from check_cassandra import check_cassandra_main
+from check_mongodb import check_mongodb_main
 
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import BranchPythonOperator
+from airflow.operators.python_operator import PythonOperator
 
 start_date = datetime(2022, 10, 19, 12, 20)
 
@@ -17,12 +23,15 @@ default_args = {
 
 with DAG('airflow_kafka_nosql', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
 
-    create_new_topic = BranchPythonOperator(task_id='create_new_topic', python_callable=create_new_topic,
+    create_new_topic = BranchPythonOperator(task_id='create_new_topic', python_callable=kafka_create_topic_main,
                              retries=2, retry_delay=timedelta(seconds=10),
                              execution_timeout=timedelta(seconds=10))
+    
+    kafka_consumer_cassandra = Python
 
     topic_created = DummyOperator(task_id="topic_created")
 
     topic_already_exists = DummyOperator(task_id="topic_already_exists")
 
     create_new_topic >> [topic_created, topic_already_exists]
+    [topic_created, topic_already_exists] >>
