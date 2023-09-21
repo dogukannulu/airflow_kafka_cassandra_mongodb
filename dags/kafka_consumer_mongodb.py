749b952edc3dc34733cf95aea3c0f9268f2dde6b
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-22 01:28:41 +0300

    add time limit

--- dags/kafka_consumer_mongodb.py
+++ dags/kafka_consumer_mongodb.py
@@ -1,24 +1,40 @@
 from confluent_kafka import Consumer, KafkaError
 from pymongo import MongoClient
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 class MongoDBConnector:
     def __init__(self, mongodb_uri, database_name, collection_name):
         self.client = MongoClient(mongodb_uri)
         self.db = self.client[database_name]
-        self.collection = self.db[collection_name]
+        self.collection_name = collection_name
 
-    def create_database(self, database_name):
-        self.client[database_name]
-
-    def create_collection(self, collection_name):
-        self.db.create_collection(collection_name)
+    def create_collection(self):
+        # Check if the collection already exists
+        if self.collection_name not in self.db.list_collection_names():
+            self.db.create_collection(self.collection_name)
+            logger.info(f"Created collection: {self.collection_name}")
+        else:
+            logger.warning(f"Collection {self.collection_name} already exists")
 
     def insert_data(self, email, otp):
+        # Check if the document already exists
+        existing_document = self.db[self.collection_name].find_one({"email": email, "otp": otp})
+        if existing_document:
+            logger.info(f"Document with Email={email}, OTP={otp} already exists in the collection.")
+            return
+
+        # If the document doesn't exist, insert it
         document = {
             "email": email,
             "otp": otp
         }
-        self.collection.insert_one(document)
+        self.db[self.collection_name].insert_one(document)
+        logger.info(f'Inserted: Email={email}, OTP={otp}')
 
     def close(self):
         self.client.close()
@@ -29,16 +45,20 @@ class KafkaConsumerWrapperMongoDB:
         self.consumer.subscribe(topics)
 
     def consume_and_insert_messages(self):
+        start_time = time.time()
         while True:
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= 30:
+                break
             msg = self.consumer.poll(1.0)
 
             if msg is None:
                 continue
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
-                    print('Reached end of partition')
+                    logger.info('Reached end of partition')
                 else:
-                    print('Error: {}'.format(msg.error()))
+                    logger.warning('Error: {}'.format(msg.error()))
             else:
                 email = msg.key().decode('utf-8')
                 otp = msg.value().decode('utf-8')
@@ -48,20 +68,23 @@ class KafkaConsumerWrapperMongoDB:
 
                 # Insert data into MongoDB collection
                 mongodb_connector.insert_data(email, otp)
-                print(f'Received and inserted: Email={email}, OTP={otp}')
+                logger.info(f'Received and inserted: Email={email}, OTP={otp}')
 
                 return data
+            
+        mongodb_connector.close()
 
     def close(self):
         self.consumer.close()
 
 # MongoDB configuration
-mongodb_connector = MongoDBConnector('mongodb://root:root@mongo:27017/', 'email_database', 'email_collection')
-
+mongodb_uri = 'mongodb://root:root@mongo:27017/'
+database_name = 'email_database'
+collection_name = 'email_collection'
+mongodb_connector = MongoDBConnector(mongodb_uri, database_name, collection_name)
 
 def kafka_consumer_mongodb_main():
-    mongodb_connector.create_database('email_database')
-    mongodb_connector.create_collection('email_collection')
+    mongodb_connector.create_collection()
 
     # Kafka configuration
     kafka_config = {
@@ -73,15 +96,11 @@ def kafka_consumer_mongodb_main():
     # Kafka topics to subscribe to
     topics = ['email_topic']
 
-    # Create a Kafka consumer and consume/insert messages
+    # Create a Kafka consumer
     kafka_consumer = KafkaConsumerWrapperMongoDB(kafka_config, topics)
-    try:
-        kafka_consumer.consume_and_insert_messages()
-        data = kafka_consumer.consume_and_insert_messages()
-        return data
-    finally:
-        kafka_consumer.close()
-        mongodb_connector.close()
+
+    kafka_consumer.consume_and_insert_messages()
+
 
 if __name__ == '__main__':
     kafka_consumer_mongodb_main()
