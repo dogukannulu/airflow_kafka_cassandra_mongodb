623976e59e57e42927d862c09908d3425f4917c9
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-25 00:23:47 +0300

    create mongodb file

---
+++ dags/get_last_record_mongodb.py
@@ -0,0 +1,25 @@
+import pymongo
+
+def get_last_email_and_otp_mongodb():
+    mongodb_uri = 'mongodb://root:root@mongo:27017/'
+    database_name = 'email_database'
+    collection_name = 'email_collection'
+    
+    client = pymongo.MongoClient(mongodb_uri)
+    database = client[database_name]
+    
+    try:
+        collection = database[collection_name]
+        
+        last_document = collection.find_one({}, sort=[('_id', pymongo.DESCENDING)])
+        
+        if last_document:
+            email = last_document.get("email")
+            top = last_document.get("top")
+            return email, top
+        else:
+            return None, None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        client.close()
