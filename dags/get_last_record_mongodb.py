b04be59df89925a7a601f4c1b4020d1c053e1e8b
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-25 00:33:38 +0300

    add logger

--- dags/get_last_record_mongodb.py
+++ dags/get_last_record_mongodb.py
@@ -1,4 +1,9 @@
 import pymongo
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def get_last_email_and_otp_mongodb():
     mongodb_uri = 'mongodb://root:root@mongo:27017/'
@@ -16,10 +21,11 @@ def get_last_email_and_otp_mongodb():
         if last_document:
             email = last_document.get("email")
             top = last_document.get("top")
+            logger.info("Records obtained successfully")
             return email, top
         else:
             return None, None
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.warning(f"An error occurred: {e}")
     finally:
         client.close()
