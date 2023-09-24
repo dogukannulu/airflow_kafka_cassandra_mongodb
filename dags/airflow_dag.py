ef8bf3ae525a747924845933bcfd6ad64b1ddd0b
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-25 00:32:46 +0300

    import new files

--- dags/airflow_dag.py
+++ dags/airflow_dag.py
@@ -1,5 +1,4 @@
 import logging
-from cassandra.cluster import Cluster
 from datetime import datetime, timedelta
 
 from airflow import DAG
@@ -13,9 +12,10 @@ from check_mongodb import check_mongodb_main
 from kafka_producer import kafka_producer_main
 from check_cassandra import check_cassandra_main
 from kafka_create_topic import kafka_create_topic_main
-from kafka_consumer_mongodb import kafka_consumer_mongodb_main, KafkaConsumerWrapperMongoDB
-from kafka_consumer_cassandra import kafka_consumer_cassandra_main, fetch_and_insert_messages
+from kafka_consumer_mongodb import kafka_consumer_mongodb_main
+from kafka_consumer_cassandra import kafka_consumer_cassandra_main
 from get_last_record_mongodb import get_last_email_and_otp_mongodb
+from get_last_record_cassandra import get_last_email_and_otp_cassandra
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -31,41 +31,7 @@ default_args = {
 }
 
 email_mongodb, otp_mongodb = get_last_email_and_otp_mongodb()
-otp_mongodb = KafkaConsumerWrapperMongoDB.consume_and_insert_messages()['otp']
-
-email_cassandra = fetch_and_insert_messages()['email']
-otp_cassandra = fetch_and_insert_messages()['otp']
-
-
-
-def get_last_email_and_top(table_name):
-    # Cassandra connection parameters
-    cassandra_hosts = ['localhost']  # Change to your Cassandra cluster hosts
-    keyspace_name = 'your_keyspace_name'  # Change to your keyspace name
-    
-    # Connect to Cassandra cluster
-    cluster = Cluster(contact_points=cassandra_hosts)
-    session = cluster.connect(keyspace_name)
-    
-    try:
-        # Prepare a CQL query to retrieve the last row from the table
-        cql_query = f"SELECT email, top FROM {table_name} LIMIT 1 ALLOW FILTERING"
-        
-        # Execute the query
-        result = session.execute(cql_query)
-        
-        # Fetch the email and top values from the result
-        for row in result:
-            email = row.email
-            top = row.top
-            return email, top
-        
-        return None, None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        cluster.shutdown()
-
+email_cassandra, otp_cassandra = get_last_email_and_otp_cassandra()
 
 
 with DAG('airflow_kafka_cassandra_mongodb', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
