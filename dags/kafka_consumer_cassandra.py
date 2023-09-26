ced829930f3868f1237474ce57b526b8ea8116e0
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-26 03:19:55 +0300

    remove comments

--- dags/kafka_consumer_cassandra.py
+++ dags/kafka_consumer_cassandra.py
@@ -77,27 +77,20 @@ def fetch_and_insert_messages(kafka_config, cassandra_connector, topic, run_dura
 
 
 def kafka_consumer_cassandra_main():
-    # Cassandra configuration
     cassandra_connector = CassandraConnector(['cassandra'])
 
-    # Create Cassandra keyspace and table
     cassandra_connector.create_keyspace()
     cassandra_connector.create_table()
 
-    # Kafka configuration
     kafka_config = {
         'bootstrap.servers': 'kafka1:19092,kafka2:19093,kafka3:19094',
         'group.id': 'cassandra_consumer_group',
         'auto.offset.reset': 'earliest'
     }
 
-    # Kafka topic to subscribe to
     topic = 'email_topic'
-
-    # Run for 30 seconds
     run_duration_secs = 30
 
-    # Fetch and insert existing messages
     fetch_and_insert_messages(kafka_config, cassandra_connector, topic, run_duration_secs)
 
     cassandra_connector.shutdown()
