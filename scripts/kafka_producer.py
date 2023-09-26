bde076f43906d6e70714179c06140aafe7456dea
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-26 21:13:52 +0300

    40 -> 20

--- scripts/kafka_producer.py
+++ scripts/kafka_producer.py
@@ -39,7 +39,7 @@ def kafka_producer_main():
             logger.info("Produced message")
             
             elapsed_time = time.time() - start_time
-            if elapsed_time >= 40:  # Stop after 40 seconds
+            if elapsed_time >= 20:  # Stop after 20 seconds
                 break
             
             time.sleep(5)  # Sleep for 5 seconds between producing messages
