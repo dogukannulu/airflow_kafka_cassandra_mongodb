91f69ecc4801d7986c50fd3a9d15ac60e466b919
Dogukan Ulu <dogukannulu@gmail.com>
2023-09-26 03:01:56 +0300

    remove mport

--- dags/kafka_consumer_cassandra.py
+++ dags/kafka_consumer_cassandra.py
@@ -1,7 +1,6 @@
 from confluent_kafka import Consumer, KafkaError, KafkaException, TopicPartition
 from cassandra.cluster import Cluster
 import time
-import json
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
